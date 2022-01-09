d84ac289d69dcbf5536e7c4ef99986570d961d0d
jsandas <jsandas82@gmail.com>
2022-01-08 19:51:35 -0700

    reduced protocol count for github actions
    Former-commit-id: e09d8a9dfe0283eb26c02ee28d3dbbdd371d387c

--- ssl/checks_test.go
+++ ssl/checks_test.go
@@ -50,7 +50,7 @@ func TestCheck(t *testing.T) {
 	}))
 	server.TLS = &tls.Config{
 		// CipherSuites: uint16[],
-		MinVersion: uint16(VersionTLS10),
+		MinVersion: uint16(VersionTLS12),
 		MaxVersion: uint16(VersionTLS13),
 	}
 	// Start a local HTTPS server
@@ -65,7 +65,7 @@ func TestCheck(t *testing.T) {
 	l := Check(host, port, "RSA")
 
 	// if less than 4 protocols supported
-	if len(l) != 4 {
-		t.Errorf("protocol count incorrect, got: %d, want: >= %d.", len(l), 3)
+	if len(l) != 2 {
+		t.Errorf("protocol count incorrect, got: %d, want: %d.", len(l), 2)
 	}
 }
