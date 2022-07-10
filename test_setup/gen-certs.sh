abd2e387ad4078d7ea3aea0c03adb689d6d4ffb8
jsandas <jsandas@users.noreply.github.com>
2022-07-10 17:46:40 -0600

    create ecc ca and entity certs
    Former-commit-id: 4f261a3b8af7456fff06bc3abe3dc0fa3e934fb7

--- test_setup/gen-certs.sh
+++ test_setup/gen-certs.sh
@@ -2,55 +2,80 @@
 
 CONF_DIR="conf/openssl"
 CA_FOLDER="ca"
-CA_SUBJECT="/CN=Self-Signed Root/C=US/ST=WA/L=Somewhere City/O=Self-Signed Inc."
+# CA_SUBJECT="/CN=Self-Signed Root/C=US/ST=WA/L=Somewhere City/O=Self-Signed Inc."
 VALID_CERT_SUBJECT="/CN=valid.tlstest.com/C=US/ST=WA/L=Somewhere City/O=Self-Signed Inc."
 REVOKED_CERT_SUBJECT="/CN=revoked.tlstest.com/C=US/ST=WA/L=Somewhere City/O=Self-Signed Inc."
 
 set -x 
 
 ca () {
-    rm -rf ca
-    mkdir -p ./$CA_FOLDER/{certs,crl,newcerts,private}
-    rm $CA_FOLDER/index*
-    touch $CA_FOLDER/index.txt
-    # touch $CA_FOLDER/index.txt.attr
-    echo 01 > $CA_FOLDER/crlnumber
-    echo 01 > $CA_FOLDER/serial
-    openssl req -nodes -x509 -config $CONF_DIR/openssl.conf -newkey rsa:2048 -keyout $CA_FOLDER/private/rootca.key -out $CA_FOLDER/certs/rootca.pem -outform PEM -days 7 -subj "$CA_SUBJECT"
-    ca_gen_crl
+    local _type=${1:-rsa}
+    rm -rf ca-$_type
+    mkdir -p ./$CA_FOLDER-$_type/{certs,crl,newcerts,private}
+    rm $CA_FOLDER-$_type/index*
+    touch $CA_FOLDER-$_type/index.txt
+    # touch $CA_FOLDER-$_type/index.txt.attr
+    echo 01 > $CA_FOLDER-$_type/crlnumber
+    echo 01 > $CA_FOLDER-$_type/serial
+    if [[ $_type == "rsa" ]]; then
+        openssl req -nodes -x509 -config $CONF_DIR/openssl-$_type.conf -newkey rsa:2048 -keyout $CA_FOLDER-$_type/private/rootca-$_type.key -out $CA_FOLDER-$_type/certs/rootca-$_type.pem -outform PEM -days 7 -subj "/CN=Self-Signed Root RSA/C=US/ST=WA/L=Somewhere City/O=Self-Signed Inc."
+        ca_gen_crl $_type
+    elif [[ $_type == "ecc" ]]; then
+        openssl req -nodes -x509 -config $CONF_DIR/openssl-$_type.conf -newkey ec:<(openssl ecparam -name secp384r1) -keyout $CA_FOLDER-$_type/private/rootca-$_type.key -out $CA_FOLDER-$_type/certs/rootca-$_type.pem -outform PEM -days 7 -subj "/CN=Self-Signed Root ECC/C=US/ST=WA/L=Somewhere City/O=Self-Signed Inc."
+        ca_gen_crl $_type
+    fi
 }
 
 ca_gen_crl () {
-    openssl ca -config $CONF_DIR/openssl.conf -gencrl -keyfile $CA_FOLDER/private/rootca.key -cert $CA_FOLDER/certs/rootca.pem -out $CA_FOLDER/crl/rootca.crl.pem
-    openssl crl -inform PEM -in $CA_FOLDER/crl/rootca.crl.pem -outform DER -out $CA_FOLDER/crl/rootca.crl
-    rm $CA_FOLDER/crl/rootca.crl.pem
-    cp $CA_FOLDER/crl/rootca.crl ../ssl/status/testrevocationfiles/test.crl
+    local _type=${1:-rsa}
+    openssl ca -config $CONF_DIR/openssl-$_type.conf -gencrl -keyfile $CA_FOLDER-$_type/private/rootca-$_type.key -cert $CA_FOLDER-$_type/certs/rootca-$_type.pem -out $CA_FOLDER-$_type/crl/rootca-$_type.crl.pem
+    openssl crl -inform PEM -in $CA_FOLDER-$_type/crl/rootca-$_type.crl.pem -outform DER -out $CA_FOLDER-$_type/crl/rootca-$_type.crl
+    rm $CA_FOLDER-$_type/crl/rootca-$_type.crl.pem
 }
 ca_sign () {
-    CERT=$1
-    openssl ca -batch -config $CONF_DIR/openssl.conf -in $CERT.csr -days 7 -extfile <(printf "subjectAltName=DNS:$CERT.tlstest.com")
-    rm $CERT.csr
+    local _cert=$1
+    local _type=${2:-rsa}
+    openssl ca -batch -config $CONF_DIR/openssl-$_type.conf -in $_cert-$_type.csr -days 7 -extfile <(printf "subjectAltName=DNS:$_cert-$_type.tlstest.com")
+    rm $_cert-$_type.csr
 }
 
 ca_revoke () {
-    CERT=$1
-    openssl ca -config $CONF_DIR/openssl.conf -revoke ca/newcerts/$CERT.pem
-    ca_gen_crl
+    local _cert=$1
+    local _type=${2:-rsa}
+    openssl ca -config $CONF_DIR/openssl-$_type.conf -revoke ca-$_type/newcerts/$_cert.pem
+    ca_gen_crl $_type
+    cp $CA_FOLDER-$_type/crl/rootca-$_type.crl ../ssl/status/testrevocationfiles/test.crl
 }
-echo " created ca..."
-ca
+echo " created certificate authorities..."
+ca rsa
 if [ $? != 0 ]; then
-    echo "something when wrong creating ca"
+    echo "something when wrong creating ca-rsa"
+    exit 1
+fi
+
+ca ecc
+if [ $? != 0 ]; then
+    echo "something when wrong creating ca-ecc"
     exit 1
 fi
 
 echo " generating certs..."
 
-openssl genrsa -out valid.key 2048 
-openssl req -config $CONF_DIR/openssl.conf -out valid.csr -key valid.key -new -sha256 -subj "$VALID_CERT_SUBJECT"
-ca_sign valid
+# Generate RSA certs
+_type=rsa
+openssl genrsa -out valid-$_type.key 2048 
+openssl req -config $CONF_DIR/openssl-$_type.conf -out valid-$_type.csr -key valid-$_type.key -new -sha256 -subj "$VALID_CERT_SUBJECT"
+ca_sign valid $_type
 
-openssl genrsa -out revoked.key 2048 
-openssl req -config $CONF_DIR/openssl.conf -out revoked.csr -key revoked.key -new -sha256 -subj "$REVOKED_CERT_SUBJECT"
-ca_sign revoked
-ca_revoke 01
+_type=rsa
+openssl genrsa -out revoked-$_type.key 2048 
+openssl req -config $CONF_DIR/openssl-$_type.conf -out revoked-$_type.csr -key revoked-$_type.key -new -sha256 -subj "$REVOKED_CERT_SUBJECT"
+ca_sign revoked $_type
+ca_revoke 01 $_type
+
+# Generate ECC certs
+_type=ecc
+openssl ecparam -name prime256v1 -genkey -noout -out valid-$_type.key
+# openssl genrsa -out valid-$_type.key 2048 
+openssl req -config $CONF_DIR/openssl-$_type.conf -out valid-$_type.csr -key valid-$_type.key -new -sha256 -subj "$VALID_CERT_SUBJECT"
+ca_sign valid $_type
