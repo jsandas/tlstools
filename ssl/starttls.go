9a0f713638ce537f08f80c013b8057eec50fbc4b
jsandas <jsandas@users.noreply.github.com>
2022-07-25 22:35:18 -0600

    refactored function and struct

--- ssl/starttls.go
+++ ssl/starttls.go
@@ -12,36 +12,14 @@ import (
 )
 
 type startTLSmsg struct {
-	greet string
-	req   string
-	resp  string
+	protocol string
+	greetMSG string
+	authMSG  string
+	respMSG  string
 }
 
-var msg = map[string]startTLSmsg{
-	"ftp": {
-		greet: "^220 ",
-		req:   "AUTH TLS\r\n",
-		resp:  "^234 ",
-	},
-	"pop3": {
-		greet: "^\\+OK ",
-		req:   "STLS\r\n",
-		resp:  "^\\+OK ",
-	},
-	"imap": {
-		greet: "^\\* ",
-		req:   "a001 STARTTLS\r\n",
-		resp:  "^a001 OK ",
-	},
-	"smtp": {
-		greet: "^220 ",
-		req:   "STARTTLS\r\n",
-		resp:  "^220 ",
-	},
-}
-
-func ehlo(w *bufio.Writer, r *bufio.Reader) (err error) {
-	var ehloStr = "ehlo TLSscanner\r\n"
+func smtpEHLO(w *bufio.Writer, r *bufio.Reader) (err error) {
+	var ehloStr = "ehlo tlstools.com\r\n"
 	var res = "^250"
 	var line string
 
@@ -69,13 +47,13 @@ func ehlo(w *bufio.Writer, r *bufio.Reader) (err error) {
 	return
 }
 
-func run(w *bufio.Writer, r *bufio.Reader, proto string) (err error) {
+func (s *startTLSmsg) connect(w *bufio.Writer, r *bufio.Reader) (err error) {
 	var line string
 
-	rgx := regexp.MustCompile(msg[proto].greet)
+	rgx := regexp.MustCompile(s.greetMSG)
 	for {
 		if line, err = r.ReadString('\n'); err != nil {
-			logger.Debugf("event_id=tcp_read_failed type=%s line=%s msg=\"%v\"", proto, strings.TrimSpace(line), err)
+			logger.Debugf("event_id=tcp_read_failed type=%s line=%s msg=\"%v\"", s.protocol, strings.TrimSpace(line), err)
 			return
 		}
 
@@ -84,25 +62,27 @@ func run(w *bufio.Writer, r *bufio.Reader, proto string) (err error) {
 		}
 	}
 
-	if err = ehlo(w, r); err != nil {
-		logger.Debugf("event_id=tcp_write_failed type=%s msg=\"%v\"", proto, err)
-		return
+	if s.protocol == "smtp" {
+		if err = smtpEHLO(w, r); err != nil {
+			logger.Debugf("event_id=tcp_write_failed type=%s msg=\"%v\"", s.protocol, err)
+			return
+		}
 	}
 
-	if _, err = w.WriteString(msg[proto].req); err != nil {
-		logger.Debugf("event_id=tcp_write_failed type=%s msg=\"%v\"", proto, err)
+	if _, err = w.WriteString(s.authMSG); err != nil {
+		logger.Debugf("event_id=tcp_write_failed type=%s msg=\"%v\"", s.protocol, err)
 		return
 	}
 	w.Flush()
 
 	if line, err = r.ReadString('\n'); err != nil {
-		logger.Debugf("event_id=tcp_read_failed type=%s msg=\"%v\"", proto, err)
+		logger.Debugf("event_id=tcp_read_failed type=%s msg=\"%v\"", s.protocol, err)
 		return
 	}
 
-	rgx = regexp.MustCompile(msg[proto].resp)
+	rgx = regexp.MustCompile(s.respMSG)
 	if !rgx.MatchString(line) {
-		logger.Debugf("event_id=starttls_not_supported server=%s line=%s msg=\"%v\"", proto, strings.TrimSpace(line), err)
+		logger.Debugf("event_id=starttls_not_supported server=%s line=%s msg=\"%v\"", s.protocol, strings.TrimSpace(line), err)
 		return errors.New("starttls_not_supported")
 	}
 
@@ -119,8 +99,38 @@ func StartTLS(conn net.Conn, port string) (err error) {
 	// "https", "imapSSL", "pop3SSL", "rdp", "smtpSSL" use regular TLS connections
 	// and are not processed further
 	switch proto {
-	case "ftp", "imap", "pop3", "smtp":
-		return run(w, r, proto)
+	case "ftp":
+		proto := startTLSmsg{
+			protocol: "ftp",
+			greetMSG: "^220 ",
+			authMSG:  "AUTH TLS\r\n",
+			respMSG:  "^234 ",
+		}
+		err = proto.connect(w, r)
+	case "imap":
+		proto := startTLSmsg{
+			protocol: "imap",
+			greetMSG: "^\\* ",
+			authMSG:  "a001 STARTTLS\r\n",
+			respMSG:  "^a001 OK ",
+		}
+		err = proto.connect(w, r)
+	case "pop3":
+		proto := startTLSmsg{
+			protocol: "pop3",
+			greetMSG: "^\\+OK ",
+			authMSG:  "STLS\r\n",
+			respMSG:  "^\\+OK ",
+		}
+		err = proto.connect(w, r)
+	case "smtp":
+		proto := startTLSmsg{
+			protocol: "smtp",
+			greetMSG: "^220 ",
+			authMSG:  "STARTTLS\r\n",
+			respMSG:  "^220 ",
+		}
+		err = proto.connect(w, r)
 	}
 
 	return
